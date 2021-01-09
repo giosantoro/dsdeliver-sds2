8a6778e30b0551d4ba25820ea8d4ecb0748808e6
Giovanni Santoro <giggio.santoro@gmail.com>
2021-01-09 10:02:05 -0300

    Add routes and Steps Header

---
+++ front-web/src/Routes.tsx
@@ -0,0 +1,22 @@
+import { Switch, BrowserRouter, Route } from "react-router-dom";
+import Orders from "./Orders";
+import Home from "./Home";
+import Navbar from "./Navbar";
+
+function Routes(){
+    return (
+        <BrowserRouter>
+            <Navbar></Navbar>
+            <Switch>
+                <Route path="/orders">
+                    <Orders />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Routes; 
