09a59959827b651decfbd3be296419c9913bb9f1
Giovanni Santoro <giggio.santoro@gmail.com>
2021-01-10 09:19:09 -0300

    Finish front-web

--- front-web/src/Orders/types.ts
+++ front-web/src/Orders/types.ts
@@ -1,13 +1,21 @@
 export type Product = {
-    id: string;
+    id: number;
     name: string;
     price: number;
     description: string;
     imageUri: string;
 }
 
-export type OrderLocationdata = {
+export type OrderLocationData = {
         address: string;
         latitude: number;
         longitude: number;
 }
+
+type ProductId = {
+    id: number;
+}
+
+export type OrderPayload = {
+    products: ProductId[];
+} & OrderLocationData;
